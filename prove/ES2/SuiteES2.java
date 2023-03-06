db800c398d9a88abfb7ff18399f05243b3c210d7
jmcq <Jcondori_0096@hotmail.com>
2023-03-06 14:48:28 +0100

    implementazione della classe-suite "SuiteES2" di ES2

---
+++ prove/ES2/SuiteES2.java
@@ -0,0 +1,8 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ CalcolatriceParametriTest.class, CalcolatriceTest.class })
+public class SuiteES2 {
+
+}
